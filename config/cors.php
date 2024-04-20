f85a2b49b4a2c2f409acc13c8f2340e050319a39
zlatangoralija <zlatan@omnitask.ba>
2024-04-20 16:02:59 +0200

    supports_credentials cors

--- config/cors.php
+++ config/cors.php
@@ -16,19 +16,11 @@ return [
     */
 
     'paths' => ['*'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => ['*'],
-
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => ['x-inertia'],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
-
+    'supports_credentials' => true,
 ];
