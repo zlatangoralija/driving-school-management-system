b948ba1d5e9f9ee0ac57932c102a68440523b6e0
zlatangoralija <zlatan@omnitask.ba>
2024-04-24 21:52:05 +0200

    Request a demo link

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -678,7 +678,7 @@ export default function Index(props) {
                 <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-16 lg:px-6">
                     <div className="max-w-screen-sm mx-auto text-center">
                         <h2 className="mb-4 text-3xl font-extrabold leading-tight tracking-tight text-gray-900">Request a demo</h2>
-                        <a href="#" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none">Request a demo now</a>
+                        <a href="https://calendly.com/asimdeveloper23" target="_blank" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none">Request a demo now</a>
                     </div>
                 </div>
             </section>
