d8d7b6a7f5ffc3ba6cc54ca391e2b3ff46b941f1
zlatangoralija <zlatan@omnitask.ba>
2024-04-29 20:17:56 +0200

    add key to mobile navigation loop

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -115,7 +115,7 @@ export default function Header(props) {
                                                     <>
                                                         {props.layout?.sidebar_menu && props.layout?.sidebar_menu.map((item,index)=>{
                                                             if(item.name !== 'Dashboard'){
-                                                                return <Link href={item.url} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{item.name}</Link>
+                                                                return <Link key={index} href={item.url} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{item.name}</Link>
                                                             }
                                                         })}
                                                     </>
