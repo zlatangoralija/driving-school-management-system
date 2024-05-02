bdd55049ad160b4d436f52e74ccb7508d71aad14
zlatangoralija <zlatan@omnitask.ba>
2024-05-02 14:03:18 +0200

    fix import?

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="{{Vite::asset('/resources/images/apple-touch-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{Vite::asset('/resources/images/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{Vite::asset('/resources/images/favicon-16x16.png')}}">
-    <link rel="manifest" href="{{Vite::asset('/resources/images/site.webmanifest')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{Vite::asset('resources/images/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{Vite::asset('resources/images/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{Vite::asset('resources/images/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{Vite::asset('resources/images/site.webmanifest')}}">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
